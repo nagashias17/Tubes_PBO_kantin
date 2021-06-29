983d180aadb1b91e2a807946265a598ae3add3e7
nagashias17 <55153193+nagashias17@users.noreply.github.com>
2021-06-29 22:21:27 +0700

    Add files via upload

---
+++ Model/main.java
@@ -0,0 +1,14 @@
+ 
+package Model;
+
+import GUI.GUI_aplikasi;
+
+
+
+public class main {
+    public static void main(String[] args) {
+       
+           GUI_aplikasi lm = new GUI_aplikasi();
+           lm.show();        
+    }
+}
